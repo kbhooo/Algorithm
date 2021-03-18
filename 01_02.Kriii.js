d2cb18dac59763096a023899ab02774f38df050f
kbhooo <ironman035614@gmail.com>
2021-03-18 21:33:15 +0900

    Modify: 01-02번 문제 작성

--- 01_02.Kriii.js
+++ 01_02.Kriii.js
@@ -3,4 +3,5 @@ const rl = readline.createInterface
 ({ input: process.stdin, output: process.stdout });
 //node.js 초기세팅
 
-console.log("Hello World!");
+console.log("강한친구 대한육군");
+console.log("강한친구 대한육군");
